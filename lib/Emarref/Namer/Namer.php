87af6acb4f74681a7b4d1141062579116930f40e
Malcolm Fell <emarref@gmail.com>
2014-05-29 13:51:14 +1200

    Refactor namer method for clarity

--- lib/Emarref/Namer/Namer.php
+++ lib/Emarref/Namer/Namer.php
@@ -60,16 +60,42 @@ class Namer
     }
 
     /**
+     * Find an available free name and return it. Returns false if no free name can be found.
+     *
+     * @param string                     $name
+     * @param Detector\DetectorInterface $detector
+     * @return string|boolean
+     */
+    private function findNewName($name, DetectorInterface $detector)
+    {
+        $i            = 1;
+        $limit        = $this->getLimit();
+        $originalName = $name;
+
+        while (!$detector->isAvailable($name)) {
+            $name = $this->strategy->getName($originalName, $i);
+
+            $i++;
+
+            if ($i > $limit) {
+                return false;
+            }
+        }
+
+        return $name;
+    }
+
+    /**
      * Given a name, use the detector to determine if the name is available to be used. If not, use the strategy to
      * generate a new name, then use the detector to determine if that name is available to be used. Continue until
      * either an available name has been found, or the limit has been reached.
      *
-     * @param string                          $name
+     * @param string                          $originalName
      * @param Detector\DetectorInterface|null $detector
      * @throws \RuntimeException
      * @return string
      */
-    public function getName($name, DetectorInterface $detector = null)
+    public function getName($originalName, DetectorInterface $detector = null)
     {
         if (!($detector instanceof DetectorInterface)) {
             $detector = $this->detector;
@@ -79,25 +105,16 @@ class Namer
             throw new \RuntimeException('No detector is configured for this namer.');
         }
 
-        $i = 1;
-        $limit = $this->getLimit();
+        $newName = $this->findNewName($originalName, $detector);
 
-        $originalName = $name;
-
-        while (!$detector->isAvailable($name)) {
-            $name = $this->strategy->getName($originalName, $i);
-
-            $i++;
-
-            if ($i > $limit) {
-                throw new \RuntimeException(sprintf(
-                    'Could not find available name for "%s" after %d attempts.',
-                    $originalName,
-                    $limit
-                ));
-            }
+        if (false === $newName) {
+            throw new \RuntimeException(sprintf(
+                'Could not find available name for "%s" after %d attempts.',
+                $originalName,
+                $this->getLimit()
+            ));
         }
 
-        return $name;
+        return $newName;
     }
 }
